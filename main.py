aa9802e7e0f56eda7380ce13b4635af19fa5b139
emovo <redsealabs@gmail.com>
2019-08-15 07:35:40 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,39 @@
+import webapp2
+import jinja2
+import os
+import datetime
+from google.appengine.ext import ndb
+from google.appengine.api import urlfetch
+from google.appengine.api import users
+from time import ctime
+import uuid
+import urllib2
+import json
+import random
+
+jinja_env=jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    undefined=jinja2.StrictUndefined, #catches template errors
+    autoescape=True
+)
+#http://127.0.0.1:8000/
+#To run the code: dev_appserver.py app.yaml
+class StartPage(webapp2.RequestHandler): #get, post
+    def get(self):
+        about_template=jinja_env.get_template('templates/start.html')
+        self.response.write(about_template.render())
+
+class InputPage(webapp2.RequestHandler):
+    def get(self): #get input
+        input_template=jinja_env.get_template('templates/input.html')
+        vars={"username":"Bob"}
+        self.response.write(about_template.render(vars))
+    def post(self): #search
+        input_template=jinja_env.get_template('templates/loading.html')
+        self.response.write(about_template.render())
+
+
+app=webapp2.WSGIApplication([
+    ('/', StartPage), ('/input', InputPage)
+], debug=True)
