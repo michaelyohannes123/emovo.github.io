1fe793f07dd7cd3bbea8c0e2180f456529bd4b94
emovo <54140311+emovo@users.noreply.github.com>
2020-09-01 11:43:33 -0400

    Added Spanish American War Battles

---
+++ spam_battles.js
@@ -0,0 +1,138 @@
+let spam_battles = 
+  {
+    "Battle of Matanzas": {
+      "date": "April 25, 1898",
+      "notes": "US victory, blockade of the Spanish port of Matanzas.[5]",
+      "campaign": "Cuba"
+    },
+    "Battle of Manila Bay": {
+      "date": "May 1, 1898",
+      "notes": "Decisive American victory, destruction of Spanish Navy′s Pacific squadron.[7]",
+      "campaign": "Philippine"
+     },
+    "First Battle of Cardenas": {
+      "date": "May 8, 1898",
+      "notes": "American victory, Spanish attempts to lift the blockade on Cuba thwarted.[6]",
+      "campaign": "Cuba"
+    },
+    "Second Battle of Cardenas": {
+      "date": "May 11, 1898",
+      "notes": "Spanish victory, loosening of the blockade on Cuba.[6]",
+      "campaign": "Cuba"
+    },
+    "Battle of Cienfuegos": {
+      "date": "May 11, 1898",
+      "notes": "Inconclusive. Americans cut telegraph cable, but suffered heavy casualties.[9]",
+      "campaign": "Cuba"
+    },
+    "Bombardment of San Juan": {
+      "date": "May 12, 1898",
+      "notes": "Spanish defenses damaged.[8]",
+      "campaign": "Puerto Rico"
+     },
+    "Battle of Guantánamo Bay": {
+      "date": "June 6–10, 1898",
+      "notes": "American victory, capture of Guantánamo Bay.[10]",
+      "campaign": "Cuba"
+    },
+    "Capture of Guam": {
+      "date": "June 20–21, 1898",
+      "notes": "US victory, capture of Guam from the Spanish.[12]",
+      "campaign": "Guam"
+    },
+    "Second Battle of San Juan": {
+      "date": "June 22, 1898",
+      "notes": "US victory, Spanish attempts to break U.S. blockade fails.[8]",
+      "campaign": "Puerto Rico"
+    },
+    "Battle of Las Guasimas": {
+      "date": "June 24, 1898",
+      "notes": "American victory, town of Sevilla captured.[11]",
+      "campaign": "Cuba"
+    },
+    "Battle of Tayacoba": {
+      "date": "June 30, 1898",
+      "notes": "Spanish victory, Americans fail to land troops.[13]",
+      "campaign": "Cuba"
+    },
+    "First Battle of Manzanillo": {
+      "date": "June 30, 1898",
+      "notes": "Spanish victory, American gunboats fail to capture Manzanillo harbor.[16]",
+      "campaign": "Cuba"
+    },
+    "Battle of El Caney": {
+      "date": "July 1, 1898",
+      "notes": "Inconclusive. American forces succeeded on capturing El Caney fort and protecting the right flank at San Juan Hill, but suffered delays and heavy casualties.[15]",
+      "campaign": "Cuba"
+    },
+    "Second Battle of Manzanillo": {
+      "date": "July 1, 1898",
+      "notes": "Spanish victory, a second attempt to capture Manzanillo harbor fails.[16]",
+      "campaign": "Cuba"
+    },
+    "Battle of San Juan Hill": {
+      "date": "July 1, 1898",
+      "notes": "American/Cuban victory, capture of San Juan heights.[14]",
+      "campaign": "Cuba"
+    },
+    "Battle of Santiago de Cuba": {
+      "date": "July 3, 1898",
+      "notes": "American victory, destruction of six Spanish ships escaping from Santiago harbor.[17]",
+      "campaign": "Cuba"
+    },
+    "Siege of Santiago de Cuba": {
+      "date": "July 3–17, 1898",
+      "notes": "American/Cuban victory, surrender of the city of Santiago de Cuba.[18]",
+      "campaign": "Cuba"
+    },
+    "Third Battle of Manzanillo": {
+      "date": "July 18, 1898",
+      "notes": "American victory, destruction of Spanish squadron in Manzanillo harbor.[16]",
+      "campaign": "Cuba"
+    },
+    "Battle of Nipe Bay": {
+      "date": "July 21, 1898",
+      "notes": "American victory, sinking of two Spanish ships.[19]",
+      "campaign": "Cuba"
+    },
+    "Battle of Mani-Mani": {
+      "date": "July 23, 1898",
+      "notes": "Spanish victory, American landing fails.[20]",
+      "campaign": "Cuba"
+    },
+    "Battle of Yauco": {
+      "date": "July 25, 1898",
+      "notes": "US victory, Spanish forces retreat and fail to destroy rail lines.[21][32]",
+      "campaign": "Puerto Rico"
+    },
+    "Battle of Guayama": {
+      "date": "August 5, 1898",
+      "notes": "US victory, capture of Guayama.[22]",
+      "campaign": "Puerto Rico"
+    },
+    "Battle of Coamo": {
+      "date": "August 8–9, 1898",
+      "notes": "US victory, capture of Coamo.[23]",
+      "campaign": "Puerto Rico"
+    },
+    "Battle of Fajardo": {
+      "date": "August 8–9, 1898",
+      "notes": "Inconclusive.[24]",
+      "campaign": "Puerto Rico"
+    },
+    "Battle of Silva Heights": {
+      "date": "August 10, 1898",
+      "notes": "US victory, capture of Silva Heights.[5]",
+      "campaign": "Puerto Rico"
+    },
+    "Battle of Mayaguez": {
+      "date": "August 11, 1898",
+      "notes": "US victory, Spanish forces retreated before the US forces arrived.[5]",
+      "campaign": "Puerto Rico"
+    },
+    "Battle of Manila": {
+      "date": "August 13, 1898",
+      "notes": "American-Filipino victory, capture of Manila.[25]",
+      "campaign": "Philippine"
+    }
+ };
---
+++ spam_battles_geo.js
@@ -0,0 +1,138 @@
+let spam_battles_geo = 
+    {
+      "Battle of Matanzas": {
+        "place": "Matanzas, Cuba",
+        "lat_lng": "23.0484426,-81.5537139",
+        "location": "water"
+      },
+      "Battle of Manila Bay": {
+        "place": "Manila Bay, Philippines, Pacific Ocean",
+        "lat_lng": "14.5,120.75",
+        "location": "water"
+       },
+      "First Battle of Cardenas": {
+        "place": "near Cárdenas, Cuba (Caribbean Sea)",
+        "lat_lng": "23.035506, -81.207503",
+        "location": "water"
+      },
+      "Second Battle of Cardenas": {
+        "place": "near Cárdenas, Cuba (Caribbean Sea)",
+        "lat_lng": "23.099834, -81.211297",
+        "location": "water"
+      },
+      "Battle of Cienfuegos": {
+        "place": "Cienfuegos, Cuba (Caribbean Sea)",
+        "lat_lng": "22.153212, -80.466673",
+        "location": "water"
+      },
+      "Bombardment of San Juan": {
+        "place": "San Juan, Puerto Rico, San Juan Bay",
+        "lat_lng": "18.4524885,-66.129134",
+        "location": "water"
+       },
+      "Battle of Guantánamo Bay": {
+        "place": "Guantánamo Bay, Cuba",
+        "lat_lng": "20.0212,-75.1137",
+        "location": "land"
+      },
+      "Capture of Guam": {
+        "place": "Guam",
+        "lat_lng": "13.4671,144.6907",
+        "location": "land"
+      },
+      "Second Battle of San Juan": {
+        "place": "Off San Juan, Cuba, (Caribbean Sea)",
+        "lat_lng": "18.458761, -66.109828",
+        "location": "water"
+      },
+      "Battle of Las Guasimas": {
+        "place": "Las Guasimas de Sevilla, Puerto Rico",
+        "lat_lng": "17.9786741,-66.0792707",
+        "location": "land"
+      },
+      "Battle of Tayacoba": {
+        "place": "near Tunas de Zaza, Cuba",
+        "lat_lng": "21.655047,-79.5771637",
+        "location": "land"
+      },
+      "First Battle of Manzanillo": {
+        "place": "Off Manzanillo, Cuba (Caribbean Sea)",
+        "lat_lng": "20.3433428,-77.156706",
+        "location": "water"
+      },
+      "Battle of El Caney": {
+        "place": "El Caney, Cuba",
+        "lat_lng": "20.0570646,-75.7757011",
+        "location": "land"
+      },
+      "Second Battle of Manzanillo": {
+        "place": "Manzanillo, Cuba",
+        "lat_lng": "20.3140551,-77.2188282",
+        "location": "water"
+      },
+      "Battle of San Juan Hill": {
+        "place": "Near Santiago de Cuba, Cuba",
+        "lat_lng": "20.020833,-75.799444",
+        "location": "land"
+      },
+      "Battle of Santiago de Cuba": {
+        "place": "Off Santiago de Cuba, Caribbean Sea",
+        "lat_lng": "20.019490, -75.844759",
+        "location": "water"
+      },
+      "Siege of Santiago de Cuba": {
+        "place": "Santiago de Cuba, Cuba",
+        "lat_lng": "20.016833, -75.821194",
+        "location": "land"
+      },
+      "Third Battle of Manzanillo": {
+        "place": "Manzanillo, Cuba",
+        "lat_lng": "20.3363587,-77.1484215",
+        "location": "water"
+      },
+      "Battle of Nipe Bay": {
+        "place": "Nipe Bay, Cuba",
+        "lat_lng": "20.7688807,-75.7547053",
+        "location": "water"
+      },
+      "Battle of Mani-Mani": {
+        "place": "Near Havana, Cuba",
+        "lat_lng": "22.961,-83.264",
+        "location": "land"
+      },
+      "Battle of Yauco": {
+        "place": "Guánica, Puerto Rico",
+        "lat_lng": "18.000907,-66.887886",
+        "location": "land"
+      },
+      "Battle of Guayama": {
+        "place": "Guayama, Puerto Rico",
+        "lat_lng": "17.974167,-66.11",
+        "location": "land"
+      },
+      "Battle of Coamo": {
+        "place": "Coamo, Puerto Rico",
+        "lat_lng": "18.08,-66.358056",
+        "location": "land"
+      },
+      "Battle of Fajardo": {
+        "place": "Fajardo, Puerto Rico",
+        "lat_lng": "18.325833,-65.6525",
+        "location": "land"
+      },
+      "Battle of Silva Heights": {
+        "place": "near Hormigueros, Puerto Rico",
+        "lat_lng": "18.085532, -67.059431",
+        "location": "land"
+      },
+      "Battle of Mayaguez": {
+        "place": "Mayagüez, Puerto Rico",
+        "lat_lng": "18.2019063,-67.1686798",
+        "location": "land"
+      },
+      "Battle of Manila": {
+        "place": "Manila, Philippines",
+        "lat_lng": "14.5677544,120.8805563",
+        "location": "land"
+      }
+    }; 
